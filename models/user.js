af32bd5058dda5a0efe02a05382316f21f0a0fb7
Dale <S00123318@mail.itsligo.ie>
2022-04-05 14:08:23 +0100

    api reg working

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     email: String,
-    password: String,
+    password: String
 });
 
 module.exports = mongoose.model('user', userSchema, 'users');
